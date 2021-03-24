3d3c3df2fae0f7ac8396b3ac40f92bc8fe36e503
SamyakManandhar <32680015+SamyakManandhar@users.noreply.github.com>
2021-03-24 11:59:48 +0000

    Add files via upload

---
+++ src/com/mi/pprank/model/Product.java
@@ -0,0 +1,40 @@
+package com.mi.pprank.model;
+
+public class Product {
+	private String productname;
+	private String description;
+	private String date;
+	private String category;
+	private String price;
+	public String getProductname() {
+		return productname;
+	}
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+}
---
+++ src/com/mi/pprank/model/Recommend.java
@@ -0,0 +1,61 @@
+package com.mi.pprank.model;
+
+public class Recommend {
+	private String fromusername;
+	private String username;
+	private String productname;
+	private String description;
+	private String date;
+	private String category;
+	private String price;
+	private int auth;
+	public int getAuth() {
+		return auth;
+	}
+	public void setAuth(int auth) {
+		this.auth = auth;
+	}
+	public String getFromusername() {
+		return fromusername;
+	}
+	public void setFromusername(String fromusername) {
+		this.fromusername = fromusername;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getProductname() {
+		return productname;
+	}
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+}
